53d328096ce28c11f4351c9044537dfa326f668c
EduardoFelipe10 <eduardo.cemer@outlook.com>
2024-09-12 19:10:09 -0300

    primeiro commit

---
+++ js/JavaScript.js
@@ -0,0 +1,10 @@
+/*var nome;
+nome = 'Eduardo Felipe!';
+alert(nome)*/
+
+var nome = prompt('insira seu nome')
+var nasci = prompt('insira seu ano de nascimento')
+var ano = 2024;
+var atual = parseInt(nasci)-parseInt(ano)
+
+alert('Olá ' + nome + ' é um prazer tê-lo (a) aqui e sua idade é: ' + atual)
